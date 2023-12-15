ac2d505fcaab2a595f35bad3d98a699e518e9430
RickS650 <141676692+RickS650@users.noreply.github.com>
2023-12-15 16:51:03 +0000

    Add files via upload

--- connect.js
+++ connect.js
@@ -1,46 +1,33 @@
-// Array of categories and their corresponding words
-const categories = [
-  { name: 'fruits', words: ['apple', 'banana', 'orange', 'grapefruit'] },
-  { name: 'animals', words: ['dog', 'elephant', 'cat', 'lion'] },
-  { name: 'objects', words: ['car', 'guitar', 'hat', 'pencil'] },
-  { name: 'nature', words: ['flower', 'moon', 'kite', 'nest'] }
+// Array of words
+const words = [
+  "Apple", "Banana", "Orange", "Grapefruit",
+  "Dog", "Elephant", "Cat", "Lion",
+  "Car", "Guitar", "Hat", "Pencil",
+  "Flower", "Moon", "Kite", "Nest"
 ];
 
-// Function to shuffle the words array
-const shuffleWords = (array) => {
+// Function to shuffle an array using the Fisher-Yates algorithm
+function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
-};
-
-// Array to store shuffled words
-const shuffledWords = [];
-
-// Loop through the categories array
-for (let i = 0; i < categories.length; i++) {
-  // Shuffle the words within each category
-  const shuffledCategoryWords = [...categories[i].words];
-  shuffleWords(shuffledCategoryWords);
-
-  // Push the shuffled words into the shuffledWords array
-  shuffledWords.push(...shuffledCategoryWords);
 }
 
-// Shuffle the words again
-shuffleWords(shuffledWords);
+// Shuffle the words array
+shuffleArray(words);
 
 // Create a container element for the grid
 const gridContainer = document.createElement('div');
 gridContainer.className = 'grid-container';
 
-// Loop through the shuffled words array
-for (let i = 0; i < shuffledWords.length; i++) {
+// Loop through the words array
+for (let i = 0; i < words.length; i++) {
   // Create a div for each word
   const wordElement = document.createElement('div');
   wordElement.className = 'word';
-  wordElement.textContent = shuffledWords[i];
-  
+  wordElement.textContent = words[i];
+
   // Add the word element to the grid container
   gridContainer.appendChild(wordElement);
 }
@@ -51,81 +38,79 @@ document.body.appendChild(gridContainer);
 // Set the CSS styles for the grid layout
 gridContainer.style.display = 'grid';
 gridContainer.style.gridTemplateColumns = 'repeat(4, 1fr)';
-gridContainer.style.gap = '10px';
+gridContainer.style.gap = '5px';
 
 // Set the CSS styles for the word elements
 const wordElements = document.querySelectorAll('.word');
 for (let i = 0; i < wordElements.length; i++) {
-  wordElements[i].style.border = '1px solid black';
-  wordElements[i].style.padding = '10px';
+  wordElements[i].style.padding = '5px';
   wordElements[i].style.textAlign = 'center';
 }
+
 // Variable to keep track of the number of selected cells
 let selectedCount = 0;
 
 // Array to store the selected cells
 const selectedCells = [];
 
-document.addEventListener('DOMContentLoaded', addCellListeners);
-
-function addCellListeners() {
-  // Get all the cells in the document
-  const cells = document.getElementsByClassName('cell');
-
-  // Add a click event listener to each cell
-  for (let i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('click', toggleSelection);
-  }
-}
-
+// Function to toggle cell selection
 function toggleSelection(event) {
   const selectedCell = event.target;
-
+  console.log(selectedCount);
   // Check if the cell is already selected
-  if (selectedCell.style.backgroundColor !== 'blue') {
+  if (!selectedCell.classList.contains('selected')) {
     // Check if the maximum number of cells has been selected
     if (selectedCount < 4) {
-      selectedCell.style.backgroundColor = 'blue';
+      selectedCell.classList.add('selected');
       selectedCount++;
       selectedCells.push(selectedCell);
+      selectedCell.style.backgroundColor = 'blue';
     }
   } else {
-    // Deselect the cell
-    selectedCell.style.backgroundColor = 'cyan';
-    selectedCount--;
-    selectedCells.splice(selectedCells.indexOf(selectedCell), 1);
+      // Deselect the cell
+      selectedCell.classList.remove('selected');
+      selectedCount--;
+      selectedCells.splice(selectedCells.indexOf(selectedCell), 1);
+      selectedCell.style.backgroundColor = 'cyan';
   }
 
   // Check if the maximum number of cells has been selected
   if (selectedCount === 4) {
-    // Disable the submit button
+    // Enable the submit button
     submitButton.disabled = false;
 
     // Remove click event listeners from other cells
-    const cells = document.getElementsByClassName('cell');
-    for (let i = 0; i < cells.length; i++) {
-      if (!selectedCells.includes(cells[i])) {
-        cells[i].removeEventListener('click', toggleSelection);
+    wordElements.forEach(cell => {
+      if (!selectedCells.includes(cell)) {
+        cell.removeEventListener('click', toggleSelection);
       }
-    }
+    });
   }
 }
 
-   
-   
-   document.addEventListener('DOMContentLoaded', function() {
-    const cells = document.querySelectorAll('.word');
-    const submitButton = document.getElementById('submit');
-   
+// Add click event listeners to the word cells
+wordElements.forEach(cell => {
+  cell.addEventListener('click', toggleSelection);
+});
 
-        let selectedCount = 0;
-    
-        cells.forEach(cell => {
-        cell.addEventListener('click', toggleSelection);
-        
-    });  
-    
-    submitButton.addEventListener('click', () => {
-    // Handle submission logic
-    });
-   });
+// Submit button functionality
+const submitButton = document.querySelector("#submit");
+submitButton.addEventListener('click', () => {
+  // Do something with the selected cells
+  console.log(selectedCells.map(cell => cell.textContent));
+
+  // Reset selection
+  selectedCount = 0;
+  selectedCells.forEach(cell => {
+    cell.classList.remove('selected');
+  });
+  selectedCells.length = 0;
+  submitButton.disabled = true;
+
+  // Re-add click event listeners to word cells
+  wordElements.forEach(cell => {
+    if (!selectedCells.includes(cell)) {
+      cell.addEventListener('click', toggleSelection);
+    }
+  });
+});
