c00f409f9b475f7b577fa02c38d3e08612ee1828
RickS650 <141676692+RickS650@users.noreply.github.com>
2024-01-03 14:54:52 +0000

    Add files via upload

--- merging.js
+++ merging.js
@@ -1,71 +1,47 @@
 // Assuming you want to merge cells
 function MergeCellsInRow(rowToMerge) {
     const rows = document.querySelectorAll('.row');
-    const cellsToMerge = rows[rowToMerge].querySelectorAll('.cell');
-    let cellX = 1;
+    //const cellsToMerge = rows[rowToMerge].querySelectorAll('.cell');
     var spanCell = document.getElementById("cell0");
+    var mergedCell = document.getElementById("cell1");
 
     switch (rowToMerge) {
       case 0:
-        var mergedCell = document.getElementById("cell"+cellX);
-        mergedCell.remove();
-        cellX++;
-        mergedCell = document.getElementById("cell"+cellX);
-        mergedCell.remove();
-        cellX++
-        mergedCell = document.getElementById("cell"+cellX);
-        mergedCell.remove();
+        for (let i = 1; i <= 3; i++) {
+          mergedCell = document.getElementById("cell"+i);
+          mergedCell.remove();
+        }
         spanCell.style.backgroundColor='lightyellow';
-        xNum = 4;
-        spanCell.removeEventListener('click', toggleSelection);
         break;
       case 1:
-        cellX = 5;
+        for (let i = 5; i <= 7; i++) {
+          mergedCell = document.getElementById("cell"+i);
+          mergedCell.remove();
+        }
         spanCell = document.getElementById("cell4");
-        var mergedCell = document.getElementById("cell"+cellX);
-        mergedCell.remove();
-        cellX++;
-        mergedCell = document.getElementById("cell"+cellX);
-        mergedCell.remove();
-        cellX++
-        mergedCell = document.getElementById("cell"+cellX);
-        mergedCell.remove();
         spanCell.style.backgroundColor="pink";
-        spanCell.removeEventListener('click', toggleSelection);
-        xNum = 8;
         break;
       case 2:
-        cellX = 9;
+        for (let i = 9; i <=11; i++) {
+          mergedCell = document.getElementById("cell"+i);
+          mergedCell.remove();
+        }
         spanCell = document.getElementById("cell8");
-        var mergedCell = document.getElementById("cell"+cellX);
-        mergedCell.remove();
-        cellX++;
-        mergedCell = document.getElementById("cell"+cellX);
-        mergedCell.remove();
-        cellX++
-        mergedCell = document.getElementById("cell"+cellX);
-        mergedCell.remove();
         spanCell.style.backgroundColor='purple';
-        xNum = 12;
-        spanCell.removeEventListener('click', toggleSelection);
+        //xNum = 12;
         break;
       case 3:
-        cellX = 13;
-        spanCell = document.getElementById("cell8");
-        var mergedCell = document.getElementById("cell"+cellX);
-        mergedCell.remove();
-        cellX++;
-        mergedCell = document.getElementById("cell"+cellX);
-        mergedCell.remove();
-        cellX++
-        mergedCell = document.getElementById("cell"+cellX);
-        mergedCell.remove();
+        for (let i = 13; i <= 15; i++) {
+          mergedCell = document.getElementById("cell"+i);
+          mergedCell.remove();
+        }
+        spanCell = document.getElementById("cell12");
         spanCell.style.backgroundColor='green';
-        xNum = 16;
-        spanCell.removeEventListener('click', toggleSelection);
+        //xNum = 16;
         break;
     }
 
+    spanCell.removeEventListener('click', toggleSelection);
     // Unable to get span to work
     //const spanCell = document.getElementById("cell0");
     //spanCell.classList.add("col-span");
@@ -74,18 +50,11 @@ function MergeCellsInRow(rowToMerge) {
     spanCell.textContent = fullCat;
 
     mergeRow ++;
-    wordElements.forEach(cell => {
-      cell.addEventListener('click', toggleSelection);
-    });
-    if (xNum< 13) {
-      reFormat(xNum);
-    } 
-
+    reFormat(mergeRow * 4);
 }
 
 function reFormat(startNumber) {
   // First remove the words already picked from the words array. ....
-  
   for (let i = 0; i < whatWord.length; i++) {
     const index = words.indexOf(whatWord[i]);
     if (index > -1) {
@@ -95,16 +64,16 @@ function reFormat(startNumber) {
 
   // then reshuffle whats left ....
   shuffleArray(words);
-
   // then reload the cells 
   for (let i = 0; i < words.length; i++) {
     var cell = document.getElementById("cell"+startNumber);
     startNumber++;
     cell.textContent = words[i];  
-    cell.style.backgroundColor = 'cyan';     
+    cell.style.backgroundColor = 'cyan';  
+    cell.addEventListener('click', toggleSelection);   
   }
-/*   submitButton.disabled = true;
-  deselectButton.disabled = true; */
+  submitButton.disabled = true;
+  deselectButton.disabled = true;
 }
  
 
