35a782d663582d82cff8e00ba73c9e2c37d65a86
RickS650 <141676692+RickS650@users.noreply.github.com>
2024-01-06 20:07:36 +0000

    Add files via upload

---
+++ heading.js
@@ -0,0 +1,7 @@
+
+window.addEventListener("load", () => {
+    const screenWidth = window.screen.width;
+    const screenHeight = window.screen.height;
+    alert(`Screen width: ${screenWidth}px`);
+    alert(`Screen height: ${screenHeight}px`);
+});
