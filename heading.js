ee7c2a3ea770cd4b1d8e7faf3867c5689467b4db
RickS650 <141676692+RickS650@users.noreply.github.com>
2024-01-17 14:34:11 +0000

    Add files via upload

--- heading.js
+++ heading.js
@@ -1,32 +1,34 @@
 
-window.addEventListener("load", () => {
+
 /*     const screenWidth = window.screen.width;
     const screenHeight = window.screen.height;
     alert(`Screen width: ${screenWidth}px`);
     alert(`Screen height: ${screenHeight}px`); */
-
-const mediaQueryPortrait = window.matchMedia("(orientation: portrait)");
-const mediaQueryLandscape = window.matchMedia("(orientation: landscape)");
-
-function handleOrientationChange() {
-  if (mediaQueryPortrait.matches) {
-    const screenWidth = window.screen.width;
-    const screenHeight = window.screen.height;
-    alert('In portrait');
-    alert(`Screen width: ${mediaQueryPortrait.screenWidth}px`);
-    alert(`Screen height: ${mediaQueryPortrait.screenHeight}px`);
-  } else if (mediaQueryLandscape.matches) {
-    // Landscape mode
-    const screenWidth = window.screen.width;
-    const screenHeight = window.screen.height;
-    alert(`Screen width: ${screenWidth}px`);
-    alert(`Screen height: ${screenHeight}px`);
-  }
-}
-});
-// Initial check
-handleOrientationChange();
-
-// Listen for orientation changes
-mediaQueryPortrait.addEventListener("change", handleOrientationChange);
-mediaQueryLandscape.addEventListener("change", handleOrientationChange);
+    window.addEventListener("load", () => {
+        const mediaQueryPortrait = window.matchMedia("(orientation: portrait)");
+        const mediaQueryLandscape = window.matchMedia("(orientation: landscape)");
+    
+        function handleOrientationChange() {
+            // Portrait mode
+            if (mediaQueryPortrait.matches) {
+                const screenWidth = window.screen.width;
+                const screenHeight = window.screen.height;
+                alert(`Portrait Screen width: ${screenWidth}px`);
+                alert(`Portrait Screen height: ${screenHeight}px`);
+            } else if (mediaQueryLandscape.matches) {
+                // Landscape mode
+                const screenWidth = window.screen.width;
+                const screenHeight = window.screen.height;
+                alert(`Landscape Screen width: ${screenWidth}px`);
+                alert(`Landscape Screen height: ${screenHeight}px`);
+            }
+        }
+    
+        // Initial check
+        handleOrientationChange();
+    
+        // Listen for orientation changes
+        mediaQueryPortrait.addEventListener("change", handleOrientationChange);
+        mediaQueryLandscape.addEventListener("change", handleOrientationChange);
+    });
+    
