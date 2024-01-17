fd6d27c59cee14d4cdc1052a39255a340258cc49
RickS650 <141676692+RickS650@users.noreply.github.com>
2024-01-17 14:18:26 +0000

    Add files via upload

--- heading.js
+++ heading.js
@@ -1,7 +1,33 @@
 
 window.addEventListener("load", () => {
+/*     const screenWidth = window.screen.width;
+    const screenHeight = window.screen.height;
+    alert(`Screen width: ${screenWidth}px`);
+    alert(`Screen height: ${screenHeight}px`); */
+
+const mediaQueryPortrait = window.matchMedia("(orientation: portrait)");
+const mediaQueryLandscape = window.matchMedia("(orientation: landscape)");
+
+function handleOrientationChange() {
+  if (mediaQueryPortrait.matches) {
+    const screenWidth = window.screen.width;
+    const screenHeight = window.screen.height;
+    alert('In portrait');
+    alert(`Screen width: ${mediaQueryPortrait.screenWidth}px`);
+    alert(`Screen height: ${mediaQueryPortrait.screenHeight}px`);
+  } else if (mediaQueryLandscape.matches) {
+    // Landscape mode
     const screenWidth = window.screen.width;
     const screenHeight = window.screen.height;
     alert(`Screen width: ${screenWidth}px`);
     alert(`Screen height: ${screenHeight}px`);
+  }
+}
+
+// Initial check
+handleOrientationChange();
+
+// Listen for orientation changes
+mediaQueryPortrait.addEventListener("change", handleOrientationChange);
+mediaQueryLandscape.addEventListener("change", handleOrientationChange);
 });
