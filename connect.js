7f06f76472a136e7c3b881bf1fcbec1f4d2cd070
RickS650 <141676692+RickS650@users.noreply.github.com>
2024-02-01 15:38:28 +0000

    Add files via upload

--- connect.js
+++ connect.js
@@ -27,7 +27,7 @@ if (notFound) {
 var date = new Date();
 var options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
 var displayDate = date.toLocaleDateString('en-GB', options);
-const existingContent = displayDate + " ver 8.s23.l";
+const existingContent = displayDate + " ver 9.s23.l";
 
 document.getElementById('datetime').innerHTML = existingContent;
 
